6ffe6045b8253a657092343603e6907e72a6a801
Kieran <belli.kieran@gmail.com>
2020-10-12 16:56:54 +0200

    armes

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
 let menuChanger = document.getElementById("menu");
-let menuRetour = document.getElementById("menu");
 menuChanger.addEventListener("mouseover", _menuChanger);
-menuRetour.addEventListener("mouseleave", _menuRetour);
+menuChanger.addEventListener("mouseleave", _menuRetour);
 
 function _menuChanger() {
-  let menu = document.getElementById("menu");
-  menu.style.display = "block";
-  menu.style.backgroundColor = " #1E90FF";
+  menuChanger.style.display = "block";
+  menuChanger.style.backgroundColor = " #1E90FF";
 }
 function _menuRetour() {
-  let menu = document.getElementById("menu");
-  menu.style.backgroundColor = "#F5DEB3";
+  menuChanger.style.backgroundColor = "#F5DEB3";
 }
