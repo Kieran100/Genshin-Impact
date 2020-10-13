c7587023f022c9046fe568aef2568843aeb46988
Kieran <belli.kieran@gmail.com>
2020-10-13 14:10:03 +0200

    un peu de jajascript

--- script.js
+++ script.js
@@ -4,8 +4,18 @@ menuChanger.addEventListener("mouseleave", _menuRetour);
 
 function _menuChanger() {
   menuChanger.style.display = "block";
-  menuChanger.style.backgroundColor = " #1E90FF";
+  menuChanger.style.fontFamily.fontcolor = "#fff";
+  menuChanger.style.backgroundColor = "  #000";
 }
 function _menuRetour() {
-  menuChanger.style.backgroundColor = "#F5DEB3";
+  menuChanger.style.backgroundColor = "rgba(255, 255, 255, .4)";
+}
+
+function openMenu() {
+  let open = document.getElementById("links");
+  if (open.style.display === "block") {
+    open.style.display = "none";
+  } else {
+    open.style.display = "block";
+  }
 }
