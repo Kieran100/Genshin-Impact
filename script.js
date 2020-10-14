79c08d93e781a232102b50d099f5721d164f65dc
Kieran <belli.kieran@gmail.com>
2020-10-14 11:36:47 +0200

    JavaScript pour le menu FAIT :)

--- script.js
+++ script.js
@@ -2,6 +2,18 @@ let menuChanger = document.getElementById("menu");
 menuChanger.addEventListener("mouseover", _menuChanger);
 menuChanger.addEventListener("mouseleave", _menuRetour);
 
+let weaponsMenu = document.getElementById("weapons");
+weaponsMenu.addEventListener("mouseover", openMenuWeapons);
+weaponsMenu.addEventListener("mouseleave", closeMenuWeapons);
+
+let persoMenu = document.getElementById("persos");
+persoMenu.addEventListener("mouseover", openMenuPersos);
+persoMenu.addEventListener("mouseleave", closeMenuPersos);
+
+let artefMenu = document.getElementById("artef");
+artefMenu.addEventListener("mouseover", openMenuArtefs);
+artefMenu.addEventListener("mouseleave", closeMenuArtefs);
+
 function _menuChanger() {
   menuChanger.style.display = "block";
   menuChanger.style.fontFamily.fontcolor = "#fff";
@@ -13,9 +25,70 @@ function _menuRetour() {
 
 function openMenu() {
   let open = document.getElementById("links");
+
   if (open.style.display === "block") {
     open.style.display = "none";
   } else {
     open.style.display = "block";
+  }
+}
+
+function closeMenuWeapons() {
+  let subMenus = document.getElementsByClassName("sous");
+  console.log(subMenus);
+  for (let index = 0; index < subMenus.length; index++) {
+    const menu = subMenus[index];
+    console.log(menu);
+    menu.style.display = "none";
+  }
+}
+
+function openMenuWeapons() {
+  let subMenus = document.getElementsByClassName("sous");
+  console.log(subMenus);
+  for (let index = 0; index < subMenus.length; index++) {
+    const menu = subMenus[index];
+    console.log(menu);
+    menu.style.display = "block";
+  }
+}
+
+function closeMenuPersos() {
+  let subMenus = document.getElementsByClassName("sousp");
+  console.log(subMenus);
+  for (let index = 0; index < subMenus.length; index++) {
+    const menu = subMenus[index];
+    console.log(menu);
+    menu.style.display = "none";
+  }
+}
+
+function openMenuPersos() {
+  let subMenus = document.getElementsByClassName("sousp");
+  console.log(subMenus);
+  for (let index = 0; index < subMenus.length; index++) {
+    const menu = subMenus[index];
+    console.log(menu);
+    menu.style.display = "block";
+  }
+}
+
+function closeMenuArtefs() {
+  let subMenus = document.getElementsByClassName("sousA");
+  console.log(subMenus);
+  for (let index = 0; index < subMenus.length; index++) {
+    const menu = subMenus[index];
+    console.log(menu);
+    menu.style.display = "none";
+  }
+}
+
+function openMenuArtefs() {
+  let subMenus = document.getElementsByClassName("sousA");
+  console.log(subMenus);
+  for (let index = 0; index < subMenus.length; index++) {
+    const menu = subMenus[index];
+    console.log(menu);
+    menu.style.display = "block";
   }
 }
